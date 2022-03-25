0bd1aa0dae8acaca5dc55bc49daec08b92085fc9
Brakhotskaya Olga <brakhotskaya.oll@gmail.com>
2022-03-25 21:51:54 +0300

    new

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 500;
+        int onemileprice = 20;
+        int bonusmiles = amount / onemileprice;
+
+        System.out.println(bonusmiles);
+
+    }
+}
