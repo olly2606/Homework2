a293176f54ecaf6b24bea2c7c3eacabcbabe6f3e
Brakhotskaya Olga <brakhotskaya.oll@gmail.com>
2022-03-30 23:50:33 +0300

    modification 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 500;
-        int onemileprice = 20;
-        int bonusmiles = amount / onemileprice;
-
-        System.out.println(bonusmiles);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 1000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
+
